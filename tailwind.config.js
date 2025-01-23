6479f02b71a37df5822de5374c438335c8f114a5
Erik <erik@botsy.org>
2025-01-23 14:32:25 +0200

    Responsive shopping list

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./**/forms.py",
     "./templates/**/*.html"
   ],
-  theme: {},
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
+  },
   plugins: [],
 }
-
