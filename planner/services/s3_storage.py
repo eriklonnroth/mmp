8fcb69b8f929b2e479023eb117cb5127599d7f0e
Erik <erik@botsy.org>
2025-01-24 19:12:55 +0200

    7 days expiry of cache and secure URL to s3

--- planner/services/s3_storage.py
+++ planner/services/s3_storage.py
@@ -7,6 +7,7 @@ class MediaStorage(S3Boto3Storage):
     bucket_name = 'erik'
     region_name = 'lon1'
     endpoint_url = 'https://lon1.digitaloceanspaces.com'
+    querystring_expire = 604800  # secure URL expires in 7 days
     object_parameters = {
-        'CacheControl': 'max-age=2592000, public'  # 30 days
+        'CacheControl': 'max-age=604800, public'  # media cache retained for 7 days on client side
     }
